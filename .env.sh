f8b5795dd8869d7aa1c03fcfac5e13235ac0b836
Clovis <51137458+munezaclovis@users.noreply.github.com>
2025-11-13 16:07:19 -0500

    Configure PNPM_HOME and update PATH settings
    Updated PNPM_HOME path and added checks for PATH exports.

--- .env.sh
+++ .env.sh
@@ -1,9 +1,21 @@
 export ZSH="$HOME/.oh-my-zsh"
 export FZF_BASE="$(brew --prefix fzf)"
 export BUN_INSTALL="$HOME/.bun"
-export PNPM_HOME="$(brew --prefix pnpm)/bin"
 
 export PATH="$BUN_INSTALL/bin:$PNPM_HOME/$HOME/.opencode/bin:$HOME/bin:$HOME/.local/bin:/usr/local/bin:/bin:$HOME/.composer/vendor/bin:$HOME/.cargo/bin:$PATH"
+
+# pnpm
+export PNPM_HOME="/opt/homebrew/opt/pnpm/bin"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
+# bun completions
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+
+# opencode
+export PATH=$HOME/.opencode/bin:$PATH
 
 export UID=$(id -u)
 export GID=$(id -g)
